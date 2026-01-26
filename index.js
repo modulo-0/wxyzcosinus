1b6a18be2b16d3a3893ae7f956fdd1e3ebe1e21c
modulo-0 <modulo0infinite@gmail.com>
2026-01-26 14:23:24 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+import connectToWhatsapp from './Digix/crew.js'
+import handleIncomingMessage from './events/messageHandler.js'
+
+{async() => {
+    await
+connectToWhatsamm(handleIncomingMessage)
+        console.log('established !')
+})()
