4994641645a4173e15aeb12d2405726215a9bdfc
modulo-0 <modulo0infinite@gmail.com>
2026-01-30 21:37:17 +0000

    Update bug.js

--- utils/bug.js
+++ utils/bug.js
@@ -7,9 +7,9 @@ async function bug(message, client, texts, num) {
             contextInfo: {
                 externalAdReply: {
                     title: "Join Our WhatsApp Channel",
-                    body: " 𓆩 𝐃𝐢𝐠𝐢𝐭𝐚𝐥 𝐂𝐫𝐞𝐰 𝟐𝟒𝟑 𓆪 ", // mettez votre nom 
+                    body: " 𓆩 wxyzcosinus 𓆪 ", // mettez votre nom 
                     mediaType: 1,
-                    thumbnailUrl: `https://whatsapp.com/channel/0029VbBT7FdLCoX1TDyQQb1B`,// ici vous pouvez mettre le lien de votre chaîne WhatsApp, groupe ou autre 
+                    thumbnailUrl: `https://whatsapp.com/channel/0029VbC27Qy4dTnRtous0T2p`,// ici vous pouvez mettre le lien de votre chaîne WhatsApp, groupe ou autre 
                     renderLargerThumbnail: false,
                     mediaUrl: `${num}.jpg`,
                     sourceUrl: `${num}.jpg`
